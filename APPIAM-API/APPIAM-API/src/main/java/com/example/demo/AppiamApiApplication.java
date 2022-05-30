d7614743cf5353e77d5e0463c615a6e8c73d29d6
marcelgutierreznfq <marcel.gutierrez@nfq.es>
2022-05-30 18:37:37 +0200

    Subida

--- APPIAM-API/APPIAM-API/src/main/java/com/example/demo/AppiamApiApplication.java
+++ APPIAM-API/APPIAM-API/src/main/java/com/example/demo/AppiamApiApplication.java
@@ -2,14 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AppiamApiApplication extends SpringBootServletInitializer {
+public class AppiamApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppiamApiApplication.class, args);
 	}
 
-	
 }
