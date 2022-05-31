a36971ddc84bb2b6a4c885d0242c1cd5cb5aa156
marcelgutierreznfq <marcel.gutierrez@nfq.es>
2022-05-31 13:27:07 +0200

    Cambio arranque

--- APPIAM-API/APPIAM-API/src/main/java/com/example/demo/AppiamApiApplication.java
+++ APPIAM-API/APPIAM-API/src/main/java/com/example/demo/AppiamApiApplication.java
@@ -2,9 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class AppiamApiApplication {
+public class AppiamApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppiamApiApplication.class, args);
